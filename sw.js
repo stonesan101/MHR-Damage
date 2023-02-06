ee846d49d6378314d95eda11c8c9541d9be31a1d
Eric Stone <estonesan88.es@gmail.com>
2023-02-06 06:33:03 -0600

    various bugs

--- sw.js
+++ sw.js
@@ -10,51 +10,52 @@
 // );
 // });
 //
-const CACHE_NAME = "my-image-cache";
-const IMAGE_URLS = [
-  "./icons/ramp1.png",
-  "./icons/ramp2.png",
-  "./icons/ramp3.png",
-  "./icons/Lvl1.png",
-  "./icons/Lvl2.png",
-  "./icons/Lvl3.png",
-  "./icons/Lvl4.png",
-  "./icons/chest.png",
-  "./icons/leg.png",
-  "./icons/helm.png",
-  "./icons/waist.png",
-  "./icons/arm.png",
-  "./icons/charm.png",
-  "./icons/gs.png",
-];
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(async (cache) => {
-      for (let url of IMAGE_URLS) {
-        if (!(await cache.match(url))) {
-          await cache.add(url);
-        }
-      }
-    })
-  );
-});
-
+// const CACHE_NAME = "my-image-cache";
+// const IMAGE_URLS = [
+  // "./icons/ramp1.png",
+  // "./icons/ramp2.png",
+  // "./icons/ramp3.png",
+  // "./icons/Lvl1.png",
+  // "./icons/Lvl2.png",
+  // "./icons/Lvl3.png",
+  // "./icons/Lvl4.png",
+  // "./icons/chest.png",
+  // "./icons/leg.png",
+  // "./icons/helm.png",
+  // "./icons/waist.png",
+  // "./icons/arm.png",
+  // "./icons/charm.png",
+  // "./icons/gs.png",
+// ];
+// self.addEventListener("install", (event) => {
+  // event.waitUntil(
+    // caches.open(CACHE_NAME).then(async (cache) => {
+      // for (let url of IMAGE_URLS) {
+        // if (!(await cache.match(url))) {
+          // await cache.add(url);
+        // }
+      // }
+    // })
+  // );
+// });
+// 
 // Listen for the fetch event and respond with the cached image if found
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
-});
-const unregisterServiceWorker = async () => {
-  if ("serviceWorker" in navigator) {
-    const registration = await navigator.serviceWorker.getRegistration();
-    if (registration) {
-      registration.unregister();
-    }
-  }
-};
+// self.addEventListener("fetch", (event) => {
+  // event.respondWith(
+    // caches.match(event.request).then((response) => {
+      // if (response) {
+        // return response;
+      // }
+      // return fetch(event.request);
+    // })
+  // );
+// });
+// const unregisterServiceWorker = async () => {
+  // if ("serviceWorker" in navigator) {
+    // const registration = await navigator.serviceWorker.getRegistration();
+    // if (registration) {
+      // registration.unregister();
+    // }
+  // }
+// };
+// 
